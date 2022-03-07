8950142212aa83c61979c9716c3208a861075997
Haidi Carolina <64173541+haidicarolina@users.noreply.github.com>
2022-03-07 20:05:09 -0300

    Daily Coding Problem #1

---
+++ DailyCodingProgram1.js
@@ -0,0 +1,21 @@
+/*  #dailycodingproblem1
+    Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+    For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+    Bonus: Can you do this in one pass?
+ */
+
+function powerSet(numbers, k) {
+    const differenceSet = new Set();
+    for (let i = 0; i < numbers.length; i++) {
+      const number = numbers[i];
+      const difference = k - number;
+      if (differenceSet.has(difference)) {
+        return true;
+      }
+      differenceSet.add(number);
+    }
+    return false;    
+}
+
+console.log('Contém ' + powerSet([1,2,3,4],5));
+console.log('Não contém ' + powerSet([1,2,3,4],5));
